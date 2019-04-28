335f54ac6286051f07225f35b07908d46a748402
Senama <sergeenama@gmail.com>
2019-04-28 13:21:40 -0400

    added navbar.css and modified login and signup

--- src/containers/signup.js
+++ src/containers/signup.js
@@ -1,15 +1,29 @@
-import React from 'react';
+// Import Routes
+
+import React, { Component } from 'react';
+// import '../signup/signup.css';
 import firebase from '../firebase';
 import AuthContext from '../contexts/auth';
 import { Redirect } from 'react-router-dom';
 
-export default class Signup extends React.Component {
 
-  state = {
-    email: '',
-    password: '',
-    error: ''
+
+
+
+// State component
+
+class Signup extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      username: '',
+      password: '',
+      error: ''
+    }
   }
+
+  // Email and Password Input Handle
 
   handleChange = (e) => {
     this.setState({ [e.target.name]: e.target.value });
@@ -17,9 +31,10 @@ export default class Signup extends React.Component {
 
   handleSubmit = (e) => {
     e.preventDefault();
+    // console.log(this.state);
 
-    const { email, password } = this.state;
-    firebase.auth().createUserWithEmailAndPassword(email, password)
+    const { username, password } = this.state;
+    firebase.auth().createUserWithEmailAndPassword(username, password)
       .then((response) => {
         console.log('Returns: ', response);
       })
@@ -29,37 +44,56 @@ export default class Signup extends React.Component {
       })
   }
 
-  render() {
-    const { email, password, error } = this.state;
-    const displayError = error === '' ? '' : <div className="alert alert-danger" role="alert">{error}</div>
-    const displayForm = <>
-      <h1>Sign Up</h1>
-      {displayError}
-      <form>
-        <div className="form-group">
-          <label htmlFor="exampleInputEmail1">Email</label>
-          <input type="email" className="form-control" aria-describedby="emailHelp" placeholder="Enter email" name="email" value={email} onChange={this.handleChange} />
-        </div>
-        <div className="form-group">
-          <label htmlFor="exampleInputPassword1">Password</label>
-          <input type="password" className="form-control" placeholder="Password" value={password} name="password" onChange={this.handleChange} />
-        </div>
-        <button type="submit" className="btn btn-primary" onClick={this.handleSubmit}>Sign Up</button>
-      </form>
-    </>;
 
+  // Render Page
+
+  render() {
     return (
       <AuthContext.Consumer>
         {
           (user) => {
             if (user) {
-              return <Redirect to='/' />
+              return (
+                <Redirect to='/' />
+              )
             } else {
-              return displayForm;
-            } 
+              return (
+                <>
+                  <div className='mx-auto d-block' style={{ 'maxWidth': '350px' }}>
+
+                    <div className="card-body text-dark">
+                      <div className='title text-center'>
+                        <h2>SIGNUP PAGE</h2>
+                      </div>
+                      <br />
+                      <br />
+                      <div>
+                        <label className="sr-only">Email</label>
+                        <input type="email" className="form-control" id="inputEmail2" placeholder="Email..." name='username' onChange={this.handleChange} value={this.state.email}></input>
+                      </div>
+                      <br />
+                      <div>
+                        <label className="sr-only">Password</label>
+                        <input type="password" className="form-control" id="inputPassword2" placeholder="Password..." name='password' onChange={this.handleChange} value={this.state.password}></input>
+                      </div>
+                      <br />
+                      <br />
+                      <div className='buttons text-center mb-5'>
+                        <button type="button" className="btn btn-danger" onClick={this.handleSubmit}>SIGNUP</button>
+                      </div>
+                    </div>
+                  </div>
+
+                </>
+              )
+            }
           }
         }
       </AuthContext.Consumer>
-    );
+
+
+    )
+
   }
 }
+export default Signup;
